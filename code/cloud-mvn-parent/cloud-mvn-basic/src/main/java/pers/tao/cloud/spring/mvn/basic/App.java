37ee9293c8472438f86251c4a4e9a98578aa1a24
liutao1024 <979497772@qq.com>
2019-01-31 10:26:59 +0800

    update 20190131AM@10:26 更新项目名称以及目录结构

---
+++ code/cloud-mvn-parent/cloud-mvn-basic/src/main/java/pers/tao/cloud/spring/mvn/basic/App.java
@@ -0,0 +1,13 @@
+package pers.tao.cloud.spring.mvn.basic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
