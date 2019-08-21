05ca3721ebee8828d8d57aa5c79f5b8121a350c2
liutao1024 <979497772@qq.com>
2019-08-21 17:54:07 +0800

    update 20190821PM@17:53 zookeeper更新

--- code/cloud-mvn-parent/cloud-mvn-zookeeper/src/test/java/cn/spring/mvn/zookeeper/ZookeeperTest.java
+++ code/cloud-mvn-parent/cloud-mvn-zookeeper/src/test/java/cn/spring/mvn/zookeeper/ZookeeperTest.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class ZookeeperTest {
 	public static void main(String[] args) throws Exception {
-		Zookeeper zookeeper = new Zookeeper();
+		ZookeeperWatcher zookeeper = new ZookeeperWatcher();
 		zookeeper.connectZookeeper("160.161.12.179:2181");
 		List<String> list = zookeeper.getChildren("/");
 		System.out.println(list);
