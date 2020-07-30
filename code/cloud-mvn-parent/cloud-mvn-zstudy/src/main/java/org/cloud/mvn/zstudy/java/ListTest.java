2a49812bef445c79e0e8dc9a95a54fa476b1f0f8
liutao <979497772@qq.com>
2020-07-30 22:55:25 +0800

    update

--- code/cloud-mvn-parent/cloud-mvn-zstudy/src/main/java/org/cloud/mvn/zstudy/java/ListTest.java
+++ code/cloud-mvn-parent/cloud-mvn-zstudy/src/main/java/org/cloud/mvn/zstudy/java/ListTest.java
@@ -47,4 +47,14 @@ public class ListTest {
 		}
 		
 	} 
+	
+	public static void listIteratorTest(){
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(12);
+		list.add(8);
+		list.add(-2);
+		list.add(-7);
+		
+		
+	}
 }
