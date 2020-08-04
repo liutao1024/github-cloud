1b33b2e27671ab586db6aba812b461919aad6d16
liutao1024 <979497772@qq.com>
2020-08-04 17:41:53 +0800

    commit

--- code/cloud-mvn-parent/cloud-mvn-zstudy/src/main/java/org/cloud/mvn/zstudy/java/MapTest.java
+++ code/cloud-mvn-parent/cloud-mvn-zstudy/src/main/java/org/cloud/mvn/zstudy/java/MapTest.java
@@ -1,36 +1,49 @@
 package org.cloud.mvn.zstudy.java;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.Map;
 import java.util.Map.Entry;
-
-import javax.persistence.Entity;
+import java.util.Set;
 
 public class MapTest {
 	public static void main(String[] args) {
 		hashMapAndHashtableTest();
 	}
+	/**
+	 * @Author LiuTao @Date 2020年8月4日 上午9:09:58 
+	 * @Title: hashMapAndHashtableTest 
+	 * @Description: TODO(Describe)
+	 */
 	public static void hashMapAndHashtableTest(){
 		Hashtable<String, Object> hashTable = new Hashtable<String, Object>();
 		Map<String, Object> hashMap = new HashMap<String, Object>();
 		Map<String, Object> hashtable = new Hashtable<String, Object>();
 		hashMap.put("Name", "LiuTao");
-		hashMap.put("Age", 24);
+		hashMap.put("Age", 8);
 		hashMap.put("Sex", "man");
-		hashMap.put("Weight", "24");
+		hashMap.put("Weight", "8");
 		System.out.println(hashMap.containsValue(24));
+		Set<String> keySet = hashMap.keySet();
+		System.out.println(keySet);
 		for (String key : hashMap.keySet()) {
 			hashMap.get(key);
 		}
 //		for (Object value : hashMap.values()) {
 //			String key = hashMap.
 //		}
+		Set<Entry<String, Object>> e = hashMap.entrySet();
+		System.out.println(e);
 		for (Entry<String, Object> entry : hashMap.entrySet()) {
-			entry.getKey();
-			entry.getValue();
+			String key = entry.getKey();
+			Object value = entry.getValue();
+			if("Age".equals(key))
+				entry.setValue("24");
 		}
-		
-		
+		System.out.println(hashMap.containsKey("Birth"));
+		System.out.println(hashMap.containsValue(8));
+		Collection<Object> cl = hashMap.values();
+		System.out.println(cl);
 	}
 }
