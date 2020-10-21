0789fccb429fc32ce5324d6e4116655a144cd792
liutao1024 <979497772@qq.com>
2020-10-21 17:42:36 +0800

    update

---
+++ code/cloud-mvn-parent/cloud-mvn-zzspring/src/main/java/org/cloud/mvn/spring/SpringApp.java
@@ -0,0 +1,16 @@
+package org.cloud.mvn.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ * Spring 学习网址: https://www.w3cschool.cn/wkspring/f8pc1hae.html
+ */
+public class SpringApp {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("SpringBeans.xml");
+		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+		obj.getMessage();
+	}
+}
