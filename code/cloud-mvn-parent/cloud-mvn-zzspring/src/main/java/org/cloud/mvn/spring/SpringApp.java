fa538fd60acdec4b4c3c76bdf608f199bae5f77b
liutao1024 <979497772@qq.com>
2020-10-21 17:47:21 +0800

    20201021@17:47

--- code/cloud-mvn-parent/cloud-mvn-zzspring/src/main/java/org/cloud/mvn/spring/SpringApp.java
+++ code/cloud-mvn-parent/cloud-mvn-zzspring/src/main/java/org/cloud/mvn/spring/SpringApp.java
@@ -7,6 +7,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * Hello world!
  * Spring 学习网址: https://www.w3cschool.cn/wkspring/f8pc1hae.html
  */
+@SuppressWarnings("resource")
 public class SpringApp {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("SpringBeans.xml");
