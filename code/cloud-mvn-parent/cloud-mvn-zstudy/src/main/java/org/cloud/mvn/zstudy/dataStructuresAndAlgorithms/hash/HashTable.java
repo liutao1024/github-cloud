8b9768947046da4e1e752898de73cbc6913c5650
liutao1024 <979497772@qq.com>
2020-11-13 17:34:52 +0800

    hashTable

--- code/cloud-mvn-parent/cloud-mvn-zstudy/src/main/java/org/cloud/mvn/zstudy/dataStructuresAndAlgorithms/hash/HashTable.java
+++ code/cloud-mvn-parent/cloud-mvn-zstudy/src/main/java/org/cloud/mvn/zstudy/dataStructuresAndAlgorithms/hash/HashTable.java
@@ -1,5 +1,12 @@
 package org.cloud.mvn.zstudy.dataStructuresAndAlgorithms.hash;
 
 public class HashTable {
-
+	public static int hashFunction(String key) {
+		int hashVal = 0;
+		for (int i = 0; i < key.length(); i++) {
+			int letter = key.charAt(i) - 96;
+			hashVal = (hashVal * 27 + letter);
+		}
+		return hashVal;
+	}
 }
