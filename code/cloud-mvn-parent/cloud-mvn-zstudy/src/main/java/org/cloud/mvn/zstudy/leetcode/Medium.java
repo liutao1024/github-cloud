55a131d1d8464b5cad03a5c326cf057a4fe83342
liutao1024 <979497772@qq.com>
2020-11-24 17:38:10 +0800

    异位字母

--- code/cloud-mvn-parent/cloud-mvn-zstudy/src/main/java/org/cloud/mvn/zstudy/leetcode/Medium.java
+++ code/cloud-mvn-parent/cloud-mvn-zstudy/src/main/java/org/cloud/mvn/zstudy/leetcode/Medium.java
@@ -1,11 +1,29 @@
 package org.cloud.mvn.zstudy.leetcode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * @Author LiuTao @Date 2020年9月21日 下午5:12:35
  * @ClassName: Medium 
  * @Description: 中等难度
  */
 public class Medium {
+	public static void main(String[] args) {
+		String[] strs = new String[7];
+		strs[0] = "eat";
+		strs[1] = "tea";
+		strs[2] = "tan";
+		strs[3] = "ate";
+		strs[4] = "nat";
+		strs[5] = "bat";
+		strs[6] = "batx";
+		List<List<String>> list = groupAnagrams(strs);
+		System.out.println(list);
+	}
 
 	public static ListNode addTwoNumber(ListNode l1, ListNode l2) {
 		ListNode dummyHead = new ListNode(0);
@@ -26,6 +44,23 @@ public class Medium {
 	    }
 	    return dummyHead.next;
 	}
+	
+	
+	@SuppressWarnings({"rawtypes", "unchecked"})
+	public static List<List<String>> groupAnagrams(String[] strs) {
+        if (strs.length == 0)
+            return new ArrayList();
+        Map<String, List> ans = new HashMap<String, List>();
+        for (String s : strs) {
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String key = String.valueOf(ca);
+            if (!ans.containsKey(key)) 
+                ans.put(key, new ArrayList());
+            ans.get(key).add(s);
+        }
+        return new ArrayList(ans.values());
+    }
 }
 class ListNode {
 	public ListNode next;
